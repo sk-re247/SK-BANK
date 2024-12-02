5728c8c1ca5e2271fa556b908a2d7fc92853dcb5
Somukumar Ekambaram <somukumar.ekambaram@cdw.com>
2024-12-02 08:03:48 +0530

    create new api for creating account

---
+++ account-service/src/main/java/com/skcorp/skbank/account_service/AccountServiceApplication.java
@@ -0,0 +1,14 @@
+package com.skcorp.skbank.account_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class AccountServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountServiceApplication.class, args);
+	}
+
+}
